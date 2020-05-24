77f691ef7dfb524bd6d20dc70c444a23f064dc6a
Teo Orthlieb <inspirateur.pro@gmail.com>
2020-05-23 21:21:51 -0400

    3d peoples are fucking degenerates

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def plot_solve(solve):
 	with open("portals.yaml", "r") as fp:
 		portals = yaml.load(fp)
-	points = np.array([[p[1], p[0]] for p in portals.values()])/8
+	points = np.array([p for p in portals.values()])/8
 	vor = Voronoi(points)
 	start = time()
 	res = solve(vor)
@@ -17,8 +17,11 @@ def plot_solve(solve):
 	voronoi_plot_2d(vor)
 	plt.scatter(res[:, 0], res[:, 1], color='red')
 	for i, name in enumerate(portals.keys()):
-		plt.annotate(name, (points[i, 0], points[i, 1]+0.06*(vor.min_bound[0]-vor.max_bound[0])), ha="center")
+		plt.annotate(name, (points[i, 0], points[i, 1]+0.04*(vor.min_bound[0]-vor.max_bound[0])), ha="center")
 	plt.title("Optimal portal repartition")
+	plt.ylabel("z")
+	plt.xlabel("x")
+	plt.gca().invert_xaxis()
 	plt.gca().set_aspect('equal', adjustable='box')
 	plt.show()
 
