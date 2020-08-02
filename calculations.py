dee6e3160d1e2c6368084c8e33f7fa3cc8e038aa
Koupic <denis.perice@gmail.com>
2020-08-02 17:28:37 -0400

    oh c'est bien, on push un truc mais je sais plus c'est quoi pcq je l'ai pas touché depuis 2 semaines, mais en tout cas je me souviens que ça marche pas j'espère ça aide
    depuis la version encienne j'ai enlevé tout ce qui servait à rien (genre le main de Téo et les voronoi de la librairie)
    bon bah c'est cool, et bah n'hésite pas à enlever tout le texte jusqu'à enlever tout ce qui servait à rien, bon bah n'hésite pas à cliquer sur commit, en gros ce message ça sert à retrouver tout ce qui tu as fait, donc écrire de la merde ça faire que tu es perdu quand tu vas ovuloir chercher ce que tu as fait
    oui mais avec ça je vais être heureux, et c'est un peu ça le but la vie

--- calculations.py
+++ calculations.py
@@ -3,175 +3,99 @@ from tqdm import trange, tqdm
 import sys
 
 
-def build_network(portals):
+class Network:
+	def __init__(self, ovw_portals):
+		"""
+
+		:param ovw_portals: data of the overworld portals, shape (2,3)
+		:return: network and usefull data about network
+		the network is a discrete 3D network : Z^3 intersection with a bounded area in the overworld of shape (net_shape, 3)
+		"""
+		shape = np.shape(ovw_portals)
+		portals = ovw_portals.reshape(shape[0] * shape[1], shape[2])
+		x_min, y_min, z_min = np.min(portals, axis=0)
+		x_max, y_max, z_max = np.max(portals, axis=0)
+		x = np.arange(x_min - 16, x_max + 16 + 1)  # Ce 16 A OPTIMISER ET NE PAS HARDCODER
+		y = np.arange(y_min, y_max + 16 + 1)
+		z = np.arange(z_min - 16, z_max + 16 + 1)
+		self.ovw_shape = (len(x), len(y), len(z))
+		x, y, z = np.meshgrid(x, y, z, indexing='ij')
+		self.ovw_network = np.reshape(np.array([x.flatten(), y.flatten(), z.flatten()]).T, self.ovw_shape + (3,))
+		self.ovw_origin = self.ovw_network[0, 0, 0]
+		x = np.arange(np.floor(x_min/8), np.floor(x_max/8) + 1)
+		y = np.arange(y_min, y_max + 1)
+		z = np.arange(np.floor(z_min/8), np.floor(z_max/8) + 1)
+		self.neth_shape = (len(x), len(y), len(z))
+		x, y, z = np.meshgrid(x, y, z, indexing='ij')
+		self.neth_network = np.reshape(np.array([x.flatten(), y.flatten(), z.flatten()]).T, self.neth_shape + (3,))
+		self.neth_origin = self.ovw_network[0, 0, 0]
+
+
+def projection(network, ovw_portals):
 	"""
 
-	:param portals:
-	:return: discrete 3D network : Z^3 intersection with a bounded area
+	:param network: discrete 3D network : Z^3 intersection with a bounded area in the overworld of shape (net_shape, 3)
+	:param ovw_portals: data of the overworld portals, shape (number of portals, 2,3)
+	:return: float tensor of shape (number of portals, net_shape) with i th element containing the euclidian distance
+	between network and i th portal
 	"""
-	shape = np.shape(portals)
-	portals = portals.reshape(shape[0]*shape[1], shape[2])
-	x_min, y_min, z_min = np.min(portals, axis=0)
-	x_max, y_max, z_max = np.max(portals, axis=0)
-	x_list = np.arange(np.trunc(x_min), np.trunc(x_max) + 1)
-	y_list = np.arange(np.trunc(y_min), np.trunc(y_max) + 1)
-	z_list = np.arange(np.trunc(z_min), np.trunc(z_max) + 1)
-	network_shape = (len(x_list), len(y_list), len(z_list))
-	x_list, y_list, z_list = np.meshgrid(x_list, y_list, z_list, indexing='ij')
-	network = np.array([x_list.flatten(), y_list.flatten(), z_list.flatten()]).T
-	return network, network_shape
-
-
-def projection(network, portals):
-	"""
-
-	:param network:
-	:param portals:
-	:return: matrix of shape (len(network), len(portals)) with element i, j equal to euclidian distance between node i
-	and clothest point in portal j
-	"""
-	portals_min, portals_max = np.transpose(portals, (1, 0, 2))
-	points = np.transpose(np.array([network for _ in range(len(portals))]), (1, 0, 2))
-	arg = (portals_min <= points)*(points <= portals_max)
-	array = np.array([portals - node for node in network])
+	print(np.shape(network.ovw_network))
+	ovw_portals = ovw_portals + 0.5  # placing portals blocks at the middle of the blocks
+	net_length = network.ovw_shape[0]*network.ovw_shape[1]*network.ovw_shape[2]
+	ovw_network = np.reshape(network.ovw_network, (net_length, 3))
+	portals_min, portals_max = np.transpose(ovw_portals, (1, 0, 2))
+	print("saucisse")
+	points = np.transpose(np.array([ovw_network for _ in range(len(ovw_portals))]), (1, 0, 2))
+	print("saucisse1")
+	portals_arg = (portals_min <= points)*(points <= portals_max)
+	print("saucisse2")
+	array = np.array([ovw_portals - node for node in ovw_network])
+	print("saucisse3")
 	argmin = np.argmin(np.abs(array), axis=2)
 	argmax = np.logical_not(argmin)
 	arg2 = np.transpose(np.array([argmax, argmin]), (1, 2, 0, 3))
-	res = np.sum(portals*arg2, axis=2)
-	res = points*arg + res*np.logical_not(arg)
-	return np.linalg.norm(res - points, axis=2)
+	res = np.sum(ovw_portals*arg2, axis=2)
+	res = points*portals_arg + res*np.logical_not(portals_arg)
+	proj = np.transpose(np.reshape(np.linalg.norm(res - points, axis=2), network.ovw_shape + (len(ovw_portals),)), (3, 0, 1, 2))
+	return proj
 
 
-def antiprojection(network, portals):
-	"""
-
-	:param network:
-	:param portals:
-	:return: matrix of shape (len(network), len(portals)) with element i, j equal to euclidian distance between node i
-	and furthest point in portal j
-	"""
-	array = np.array([portals - node for node in network])
-	argmax = np.argmax(np.abs(array), axis=2)
-	argmin = np.logical_not(argmax)
-	arg = np.transpose(np.array([argmin, argmax]), (1, 2, 0, 3))
-	points = np.sum(portals*arg, axis=2)
-	network = np.transpose(np.array([network for _ in range(len(portals))]), (1, 0, 2))
-	return np.linalg.norm(points - network, axis=2)
-
-
-def build_region(network, network_shape, portals):
-	
-	regions = np.zeros((len(portals),) + network_shape, dtype=np.bool)
-	proj = projection(network, portals)
-	anti_proj = antiprojection(network, portals)
-	proj = np.reshape(proj, network_shape + (len(portals),))
-	anti_proj = np.reshape(anti_proj, network_shape + (len(portals),))
-	argmin = np.argmin(proj, axis=3)
-	buffer = 0
-
-	for ind_x in trange(network_shape[0], desc="building regions", file=sys.stdout):
-		for ind_y in range(network_shape[1]):
-			for ind_z in range(network_shape[2]):
-				am = argmin[ind_x, ind_y, ind_z]
-				if anti_proj[ind_x, ind_y, ind_z, am] < np.min(np.delete(proj[ind_x, ind_y, ind_z], am)) + buffer:
-					regions[am, ind_x, ind_y, ind_z] = True
+def build_region(network, ovw_portals):
+	regions = np.zeros((len(ovw_portals),) + network.ovw_shape, dtype=np.bool)
+	proj = projection(network, ovw_portals)
+	argmin = np.argmin(proj, axis=0)
+	for ix in trange(network.ovw_shape[0], desc="building regions", file=sys.stdout):
+		for iy in range(network.ovw_shape[1]):
+			for iz in range(network.ovw_shape[2]):
+				am = argmin[ix, iy, iz]
+				regions[am, ix, iy, iz] = True
 	return regions
 
 
-def possible_portails(network, network_shape, portals, regions, names):
-	proj = projection(network, portals)
-	anti_proj = antiprojection(network, portals)
-	proj = np.reshape(proj, network_shape + (len(portals),))
-	anti_proj = np.reshape(anti_proj, network_shape + (len(portals),))
-	portal_x = np.array([1, 2, 0])
-	portal_z = np.array([0, 2, 1])
-	nb = len(portals)
-	portals = [[] for _ in range(nb)]
-	origin = network[0]
-	x_shape = np.array(network_shape) - portal_x
-	xl, yl, zl = np.arange(x_shape[0]), np.arange(x_shape[1]), np.arange(x_shape[2])
-	xl, yl, zl = np.meshgrid(xl, yl, zl, indexing='ij')
-	x_network = np.transpose([xl.flatten(), yl.flatten(), zl.flatten()])
-	z_shape = np.array(network_shape) - portal_z
-	xl, yl, zl = np.arange(z_shape[0]), np.arange(z_shape[1]), np.arange(z_shape[2])
-	xl, yl, zl = np.meshgrid(xl, yl, zl, indexing='ij')
-	z_network = np.transpose([xl.flatten(), yl.flatten(), zl.flatten()])
-	for i in trange(nb, desc="finding possibles portals in regions", file=sys.stdout):
-		region = regions[i]
-		for n in x_network:
-			fn = n + portal_x + [1, 1, 1]
-			if region[n[0]:fn[0], n[1]:fn[1], n[2]:fn[2]].all():
-				# if np.max(anti_proj[n[0]:fn[0], n[1]:fn[1], n[2]:fn[2], i]) < np.min(proj[n[0]:fn[0], n[1]:fn[1], n[2]:fn[2], np.delete(range(nb), i)]):
-				portals[i].append(np.array([n, fn - [1, 1, 1]]) + origin)
-		for n in z_network:
-			fn = n + portal_z + [1, 1, 1]
-			if region[n[0]:fn[0], n[1]:fn[1], n[2]:fn[2]].all():
-				# if np.max(anti_proj[n[0]:fn[0], n[1]:fn[1], n[2]:fn[2], i]) < np.min(proj[n[0]:fn[0], n[1]:fn[1], n[2]:fn[2], np.delete(range(nb), i)]):
-				portals[i].append(np.array([n, fn - [1, 1, 1]]) + origin)
-		portals[i] = np.array(portals[i])
-	return portals
+def possible_portails(network, regions):
+	pos_portail = [[] for _ in range(len(regions))]
+	for i in trange(len(regions), desc="finding possibles portals in regions", file=sys.stdout):
+		for ix in range(network.neth_shape[0]):
+			for iy in range(network.neth_shape[1]):
+				for iz in range(network.neth_shape[2]):
+					y = iy*8 - network.ovw_origin[1]
+					x_min = ix*8 - network.ovw_origin[0]
+					x_max = (ix + 2)*8 - network.ovw_origin[0]  # CES COORDONNES DOIVENT ETRE OPTIMISABLES, VOIR DANS MINECRAFT
+					z_min = np.floor((iz - 0.3)*8).astype(int) - network.ovw_origin[2]
+					z_max = np.floor((iz + 1.3)*8).astype(int) - network.ovw_origin[2]
+					if regions[i, x_min:x_max + 1, y, z_min:z_max + 1].all():
+						pos_portail[i].append(np.array([[ix, iy, iz], [ix + 1, iy + 2, iz]]))
+					x_min = np.floor((ix - 0.3)*8).astype(int) - network.ovw_origin[0]
+					x_max = np.floor((ix + 1.3)*8).astype(int) - network.ovw_origin[0]
+					z_min = iz*8 - network.ovw_origin[2]
+					z_max = (iz + 2)*8 - network.ovw_origin[2]
+					if regions[x_min:x_max + 1, y, z_min:z_max + 1].all():
+						pos_portail[i].append(np.array([[ix, iy, iz], [ix, iy + 2, iz + 1]]))
+	return pos_portail
 
 
-def project(portals, m):
-	norm = [np.linalg.norm(np.mean(portals_in_reg, axis=1) - m, axis=1) for portals_in_reg in portals]
+def project(pos_portail, m):
+	norm = [np.linalg.norm(np.mean(portals_in_reg, axis=1) - m, axis=1) for portals_in_reg in pos_portail]
 	argmin = [np.argmin(norm_in_reg) for norm_in_reg in norm]
-	portals = np.array([portals[i][argmin[i]] for i in range(len(portals))])
-	return portals
-
-
-def checking(res_portals, portals, names):
-	nb = len(portals)
-	for i in trange(nb, desc="testing result", file=sys.stdout):
-		res = get_all_portal(res_portals[i])
-		for res_block in res:
-			dist_max = 0
-			for portal_block in get_all_portal(portals[i]):
-				dist = np.linalg.norm(res_block - portal_block)
-				if dist_max > dist:
-					dist_max = dist
-			dist_min = np.inf
-			for j in np.delete(range(nb), i):
-				for portal_block in get_all_portal(portals[j]):
-					dist = np.linalg.norm(res_block - portal_block)
-					if dist_min < dist:
-						dist_min = dist
-			if not(dist_max < dist_min):
-				raise ValueError(f"Portail {names[i]}, precisely block {res_block} is not in the proper region")
-	print("no portals are in the MOJANG YOU RETARD border, and all portals are in the good region")
-
-
-# def checking(res_portals, portals, names):
-# 	nb = len(portals)
-# 	min_dist = np.ones((nb, nb)) * np.inf
-# 	max_dist = np.zeros((nb, nb))
-# 	for i in trange(nb, desc="testing result", file=sys.stdout):
-# 		res = get_all_portal(res_portals[i])
-# 		for j in range(nb):
-# 			portal = get_all_portal(portals[j])
-# 			for res_block in res:
-# 				for portal_block in portal:
-# 					dist = np.linalg.norm(res_block - portal_block)
-# 					if dist < min_dist[i, j]:
-# 						min_dist[i, j] = dist
-# 					if dist > max_dist[i, j]:
-# 						max_dist[i, j] = dist
-# 	argmin = np.argmin(min_dist, axis=1)
-# 	for i in range(nb):
-# 		if argmin[i] != i:
-# 			raise ValueError(f"{names[i]} portal is not is the right region")
-# 	print("all portals are in the good regions")
-# 	for i in range(nb):
-# 		if not(max_dist[i, i] < np.min(np.delete(min_dist[i], i))):
-# 			print("max dist to portals = ", max_dist[i])
-# 			print("min dist to portals = ", min_dist[i])
-# 			print("no good NO GOOD NONONONONO :", res_portals[i])
-# 			raise ValueError(f"{names[i]} portal is in the MOJANG YOU RETARD border")
-# 	print("no portals are in the MOJANG YOU RETARD border \n  -------------------- ")
-
-
-def get_all_portal(portal):
-	delta = portal[1] - portal[0]
-	if delta[0] == 0:
-		return np.array([portal[0], portal[0] + [0, 0, delta[2]], portal[0] + [0, 1, 0], portal[0] + [0, 1, delta[2]], portal[0] + [0, 2, 0], portal[1]])
-	else:
-		return np.array([portal[0], portal[0] + [delta[0], 0, 0], portal[0] + [0, 1, 0], portal[0] + [delta[0], 1, 0], portal[0] + [0, 2, 0], portal[1]])
+	portals_found = np.array([pos_portail[i][argmin[i]] for i in range(len(pos_portail))])
+	return portals_found
--- main.py
+++ main.py
@@ -1,47 +1,18 @@
-from time import time
 import yaml
-from scipy.spatial import Voronoi, voronoi_plot_2d
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+from solver import find_portals
 from loss import mean_square
+from time import time
 
 
-def plot_solve(solve):
-	with open("portals.yaml", "r") as fp:
-		portals = yaml.load(fp)
-	points = np.array([p for p in portals.values()])
-	points = np.transpose(points, (2, 0, 1))
-	points = np.transpose(np.array([points[0]/8, points[1], points[2]/8]), (1, 2, 0))
-	xz_points = np.mean(np.array([points.T[2], points.T[0]]).T, axis=1)
-	vor = Voronoi(xz_points)
-	start = time()
-	res = solve(points, list(portals.keys()))
-	delta = time()-start
-	maxlen = max(len(name) for name in portals.keys())+1
-	for name, p in zip(portals.keys(), res):
-		print(f"{(name+':').ljust(maxlen)} {p[0]} --> {p[1]}")
-	print(f"loss {mean_square(res):.1f} (in {int(delta*1000)} ms)")
-	fig, ax = plt.subplots()
-	voronoi_plot_2d(vor, ax, show_vertices=False)
-	res_2d = np.mean(np.array([res.T[2], res.T[0]]).T, axis=1)
-	plt.scatter(res_2d[:, 0], res_2d[:, 1], marker="x", color='red', zorder=10)
-	x_min, y_min = np.min(res_2d, axis=0)
-	x_max, y_max = np.max(res_2d, axis=0)
-	x_min2, x_max2 = plt.gca().get_xlim()
-	y_min2, y_max2 = plt.gca().get_ylim()
-	plt.xlim(min(x_min, x_min2), max(x_max, x_max2))
-	plt.ylim(min(y_min, y_min2), max(y_max, y_max2))
-	for i, name in enumerate(portals.keys()):
-		plt.annotate(name, (xz_points[i, 0], xz_points[i, 1]+0.03*(vor.min_bound[0]-vor.max_bound[0])), ha="center")
-	plt.title("Optimal portal repartition")
-	plt.xlabel("z")
-	plt.ylabel("x")
-	plt.gca().set_aspect('equal', adjustable='box')
-	plt.grid(linestyle="--")
-	ticks = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x))  # *8
-	ax.xaxis.set_major_formatter(ticks)
-	ax.xaxis.set_major_locator(plt.MultipleLocator(50.0/8))
-	ax.yaxis.set_major_formatter(ticks)
-	ax.yaxis.set_major_locator(plt.MultipleLocator(50.0/8))
-	plt.show()
+with open("overworld_portals.yaml", "r") as fp:
+	portals = yaml.load(fp)
+ovw_portals = np.array([p for p in portals.values()])
+start = time()
+neth_portals = find_portals(ovw_portals, list(portals.keys()))
+delta = time() - start
+maxlen = max(len(name) for name in portals.keys()) + 1
+print("optimal nether portals : \n")
+for name, p in zip(portals.keys(), neth_portals):
+	print(f"{(name+':').ljust(maxlen)} {p[0]} --> {p[1]}")
+print(f"\n loss {mean_square(neth_portals):.1f} (in {int(delta*1000)} ms)")
